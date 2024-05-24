01383f5616387ed73fb1af803d9c9905f70a2d19
zle <128670653+1669215953@users.noreply.github.com>
2024-05-24 19:11:19 +0800

    Add files via upload

---
+++ library/src/org/library/test/test.java
@@ -0,0 +1,23 @@
+package org.library.test;
+
+public class test {
+    public static void main(String[] args) {
+//        new Thread(() -> {
+//            if(Sys.login(0, "12", "1007"))
+//            Sys.getUser().borrowBook("bookName", "nihao");
+//        }).start();
+//
+//        new Thread(() -> {
+//            if(Sys.login(0, "123", "1006"))
+//                Sys.getUser().borrowBook("bookName", "世界");
+//        }).start();
+//        User user = new User("123", "123", "34");
+//        user.returnBook();
+//        user.borrowBook("ISBN", "789456");
+//        Map<String, Object> tmp = DatabaseConnector.search_user("123").get(0);
+//        System.out.println(tmp);
+//        String str = (String) tmp.get("id");
+//        System.out.println("123".equals(str));
+//        DatabaseConnector.update_book("123456", "benyoung");
+    }
+}
